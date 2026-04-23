657f8e79c5ebb8d2551bd9cb67e44e08d259abbd
San097 <santoshkumarg112003@gmail.com>
2026-04-23 16:32:16 +0530

    Road_pavement_project121

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-gunicorn --bind 0.0.0.0:$PORT --workers $WEB_CONCURRENCY app:app
+gunicorn --bind 0.0.0.0:$PORT --workers ${WEB_CONCURRENCY:-1} road_pavement_project.app:app
---
+++ your_application/wsgi.py
@@ -0,0 +1,3 @@
+from road_pavement_project.app import app
+
+application = app
